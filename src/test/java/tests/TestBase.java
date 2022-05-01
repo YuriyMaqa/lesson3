acada37c2079d1cc7572eb4bdd94e57a04e04f8f
YuriyMaqa <yuriymartynovqa@gmail.com>
2022-05-02 00:58:44 +0300

    owner add

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,14 +13,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
     @BeforeAll
     static void setUp() {
-        String selenoidUrl = System.getProperty("selenoid", "selenoid.autotests.cloud");
-        String propertyBrowserSize = System.getProperty("propertyBrowserSize","1980x1024");
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = "https://demoqa.com";
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+        String login = config.loginSelenoid();
+        String password = config.passwordSelenoid();
+
+        String selenoidUrl = System.getProperty("selenoid", "selenoid.autotests.cloud/wd/hub");
+        String propertyBrowserSize = System.getProperty("propertyBrowserSize","1980x1024");
+        String startUrl = System.getProperty("startUrl", "https://demoqa.com");
+
+
+
+        Configuration.baseUrl = startUrl;
         Configuration.browserSize = propertyBrowserSize;
-        Configuration.remote = "https://user1:1234@" + selenoidUrl + "/wd/hub";
+        Configuration.remote = "https://"+ login + password + selenoidUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
